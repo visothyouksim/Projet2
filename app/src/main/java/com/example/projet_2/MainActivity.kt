d7e359a89e8fd47c104529a130336e2185d55317
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-06-12 16:04:46 +0200

    test_formulaire

--- app/src/main/java/com/example/projet_2/MainActivity.kt
+++ app/src/main/java/com/example/projet_2/MainActivity.kt
@@ -3,16 +3,31 @@ package com.example.projet_2
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editText: EditText
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.btnPage2).setOnClickListener{
-            val intentNextActivity : Intent = Intent(this, PageTwo::class.java)
+            val intentNextActivity = Intent(this, PageTwo::class.java)
             startActivity(intentNextActivity)
+        }
+
+        editText = findViewById(R.id.editText)
+        button = findViewById(R.id.btnSaveText)
+
+        button.setOnClickListener {
+            val text = editText.text.toString()
+            val intent = Intent(this, PageTwo::class.java).apply {
+                putExtra("TEXT", text)
+            }
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/projet_2/PageTwo.kt
+++ app/src/main/java/com/example/projet_2/PageTwo.kt
@@ -1,18 +1,31 @@
 package com.example.projet_2
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class PageTwo : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page_two)
+
+        println("ATTENTION PRINT")
+        println(intent.getIntExtra("id", 0))
 
         findViewById<Button>(R.id.btnPage3).setOnClickListener{
             val intentNextActivity : Intent = Intent(this, PageThree::class.java)
             startActivity(intentNextActivity)
         }
+
+        textView = findViewById(R.id.editText)
+        val text = intent.getStringExtra("TEXT")
+        textView.text = text
     }
 }
